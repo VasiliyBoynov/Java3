0d14ab7f43e041dfe1d1e7c22bd65570fb6e8195
Vasiliy Boynov <bot52@yandex.ru>
2021-02-27 01:13:23 +0300

    Lesson5 Для треннировки и лучшего освоения материала: Специально использовал AtomicBoolean для определения победителя. Специально использовал метод lock() в сочетании с volatile для "очень странного синглтона" Специально применял метод setCdl() для явной установки ожидаемого значения счётчика CountDownLatch Использовал Semaphore для настройки пропускной способности тонеля, добавлен счётчик для debug режима.

--- src/main/java/Lesson5/Tunnel.java
+++ src/main/java/Lesson5/Tunnel.java
@@ -1,6 +1,7 @@
 package Lesson5;
 
 public class Tunnel extends Stage {
+    private static int count;
     public Tunnel() {
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
@@ -12,11 +13,14 @@ public class Tunnel extends Stage {
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
                 Car.getSmp().acquire();
                 System.out.println(c.getName() + " начал этап: " + description);
+                count++;
+                System.out.printf("[DEBUG] В тонеле %d участников %n ",count);
                 Thread.sleep(length / c.getSpeed() * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
                 System.out.println(c.getName() + " закончил этап: " + description);
+                count--;
                 Car.getSmp().release();
             }
         } catch (Exception e) {
